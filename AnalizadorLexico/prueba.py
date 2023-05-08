44a7ae0edc87188ac12d263db36c739bf96bf951
xJuairo <xjlopz10@gmail.com>
2023-05-08 02:04:22 -0600

    Cambios2

--- AnalizadorLexico/prueba.py
+++ AnalizadorLexico/prueba.py
@@ -2,85 +2,91 @@
 import sys
 import re
 
-contenido = ""
+argumentos = ""
 for i in range(0,len(sys.argv)):
-    contenido += str(sys.argv[i])
-    contenido+='\n'
+    argumentos += str(sys.argv[i])
+    argumentos+='\n'
 
 # Diccionarios de tokens
 palabras_reservadas = {"if": "palabra reservada", "else": "palabra reservada", "end": "palabra reservada", "do": "palabra reservada", "while": "palabra reservada", "repeat": "palabra reservada", "until": "palabra reservada", "cin": "palabra reservada", "cout": "palabra reservada", "real": "palabra reservada", "int": "palabra reservada", "boolean": "palabra reservada", "true": "palabra reservada", "false": "palabra reservada"}
-simbolos_especiales = {"(": "PAR_IZQ", ")": "PAR_DER", "{": "LLAVE_IZQ", "}": "LLAVE_DER", ";": "PUNTO_COMA", ",": "COMA"}
-operadores_aritmeticos = {"+": "SUMA", "-": "RESTA", "*": "MULTIPLICACION", "/": "DIVISION", "=": "IGUALACION"}
-operadores_relacionales = {"==": "IGUALDAD", "!=": "DIFERENTE", "<>":"DIFERENTE2","<": "MENOR_QUE", ">": "MAYOR_QUE", "<=": "MENOR_IGUAL_QUE", ">=": "MAYOR_IGUAL_QUE"}
-operadores_logicos = {"&&": "AND", "||": "OR", "!": "NOT"}
-operadores_dobles = {"++": "INCREMENTO", "--": "DECREMENTO"}
-# Tokenizar contenido
+palabras_reservadas_key = palabras_reservadas.keys()
+puntuacion_grupos = {"(": "parentesis abierto", ")": "parentesis cerrado", "{": "llave abierto", "}": "llave cerrado", ";": "punto y coma", ",": "coma"}
+puntuacion_grupos_key = puntuacion_grupos.keys()
+aritmetica = {"+": "operador suma", "-": "operador resta", "*": "operador multiplicacion", "/": "operador division", "=": "operador asignacion"}
+aritmetica_key = aritmetica.keys()
+comparaciones = {"==": "operador iguales", "!=": "operador diferente", "<>":"operador diferente","<": "operador menor que", ">": "operador mayor que", "<=": "operador menor o igual que", ">=": "operador mayor o igual que"}
+comparaciones_key = comparaciones.keys()
+logicos = {"&&": "operador and", "||": "operador or", "!": "operador not"}
+logicos_key = logicos.keys()
+dobles = {"++": "operador incremento", "--": "operador decremento"}
+dobles_key = dobles.keys()
+# Tokenizar argumentos
 tokens = []
 linea = 1
 col = 1
 i = 0
-while i < len(contenido):
+while i < len(argumentos):
     # Ignorar espacios en blanco
     col+=1
-    if contenido[i].isspace():
-        if (contenido[i] == "\n"):
+    if argumentos[i].isspace():
+        if (argumentos[i] == "\n"):
             linea +=1
             col = 1
         i += 1
         continue
     # Identificar comentarios de una línea
-    if contenido[i:i+2] == "//":
-        i = contenido.index("\n", i)
+    if argumentos[i:i+2] == "//":
+        i = argumentos.index("\n", i)
         continue
     # Identificar comentarios multilinea
-    if contenido[i:i+2] == "/*":
-        i = contenido.index("*/", i) + 2
+    if argumentos[i:i+2] == "/*":
+        i = argumentos.index("*/", i) + 2
         continue
     # Identificar palabras reservadas, identificadores y números
-    if contenido[i].isalpha():
+    if argumentos[i].isalpha():
         j = i + 1
-        while j < len(contenido) and (contenido[j].isalnum() or contenido[j] == "_"):
+        while j < len(argumentos) and (argumentos[j].isalnum() or argumentos[j] == "_"):
             j += 1
-        token = contenido[i:j]
+        token = argumentos[i:j]
         if token in palabras_reservadas:
             tokens.append("[" + token + ", "  + palabras_reservadas[token] +"]")
         else:
             tokens.append("[" + token + ", ídentificador]")
         i = j
         continue
-    elif contenido[i].isdigit():
+    elif argumentos[i].isdigit():
         j = i + 1
-        while j < len(contenido) and contenido[j].isdigit():
+        while j < len(argumentos) and argumentos[j].isdigit():
             j += 1
-        if j < len(contenido) and contenido[j] == ".":
+        if j < len(argumentos) and argumentos[j] == ".":
             j += 1
-            while j < len(contenido) and contenido[j].isdigit():
+            while j < len(argumentos) and argumentos[j].isdigit():
                 j += 1
-            tokens.append("[" + contenido[i:j] + ", flotante]")
+            tokens.append("[" + argumentos[i:j] + ", flotante]")
         else:
-            tokens.append("[" + contenido[i:j] + ", entero]")
+            tokens.append("[" + argumentos[i:j] + ", entero]")
         i = j
         continue
     # Identificar símbolos especiales
-    if contenido[i] in simbolos_especiales:
-        tokens.append("[" + contenido[i] + ", simbolo especial]")
+    if argumentos[i] in puntuacion_grupos:
+        tokens.append("[" + argumentos[i] + ", simbolo especial]")
         i += 1
         continue
     # Identificar operadores aritméticos y relacionales
-    if contenido[i:i+2] in operadores_relacionales:
-        tokens.append("[" + operadores_relacionales[contenido[i:i+2]] + ", operador relacional]")
+    if argumentos[i:i+2] in comparaciones:
+        tokens.append("[" + comparaciones[argumentos[i:i+2]] + ", operador relacional]")
         i += 2
         continue
-    elif contenido[i] in operadores_relacionales:
-        tokens.append("[" + operadores_relacionales[contenido[i]] + ", operador relacional]")
+    elif argumentos[i] in comparaciones:
+        tokens.append("[" + comparaciones[argumentos[i]] + ", operador relacional]")
         i += 1
         continue
-    if contenido[i:i+2] in operadores_dobles:
-        tokens.append("[" +contenido[i:i+2] + ", operador aritmetico]")
+    if argumentos[i:i+2] in dobles:
+        tokens.append("[" +argumentos[i:i+2] + ", operador aritmetico]")
         i += 2
         continue
-    elif contenido[i] in operadores_aritmeticos:
-        tokens.append("[" + contenido[i] + ", operador aritmetico]")
+    elif argumentos[i] in aritmetica:
+        tokens.append("[" + argumentos[i] + ", operador aritmetico]")
         i +=1
         continue
     else: 
